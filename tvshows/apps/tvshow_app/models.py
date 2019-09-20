6613d74dde663a54188850bf65181820766cf5ce
Nawaf <nawafhunter@gmail.com>
2019-09-20 23:23:19 +0300

    add basic validation

--- tvshows/apps/tvshow_app/models.py
+++ tvshows/apps/tvshow_app/models.py
@@ -1,8 +1,21 @@
+from __future__ import unicode_literals
 from django.db import models
+
+
+class TV_shows_manager(models.Manager):
+    def validator(self, postedData):
+        errors = {}
+        if len(postedData['title']) < 1:
+            errors['title'] = "title cannot be empty"
+        if len(postedData['network']) < 1:
+            errors['network'] = "network cannot be empty"
+        if len(postedData['description']) < 1:
+            errors['description'] = "description cannot be empty"
+        return errors
 
 class TV_shows(models.Model):
     title = models.CharField(max_length=50)
     network = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     release_date = models.DateField()
-
+    objects = TV_shows_manager()
--- tvshows/apps/tvshow_app/views.py
+++ tvshows/apps/tvshow_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import TV_shows
 
 
@@ -14,11 +15,17 @@ def new_show_form(request):
 
 
 def add_new_show(request):
-    new_show = TV_shows.objects.create(title=request.POST['title'],
-                                       network=request.POST['network'],
-                                       description=request.POST['description'],
-                                       release_date=request.POST['release_date'])
-    return redirect("/")
+    errors = TV_shows.objects.validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/new_show_form")
+    else:
+        new_show = TV_shows.objects.create(title=request.POST['title'],
+                                        network=request.POST['network'],
+                                        description=request.POST['description'],
+                                        release_date=request.POST['release_date'])
+        return redirect("/", messages)
 
 
 def display_show_info(request, show_id):
@@ -35,12 +42,18 @@ def edit_show_form(request, show_id):
     return render(request, 'tvshow_app/edit_show.html', context)
 
 def edit_show(request, show_id):
-    edit_show = TV_shows.objects.get(id = show_id)
-    edit_show.title = request.POST['title']
-    edit_show.network = request.POST['network']
-    edit_show.description = request.POST['description']
-    edit_show.release_date = request.POST['release_date']
-    edit_show.save()
+    errors = TV_shows.objects.validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/{show_id}/edit")
+    else:
+        edit_show = TV_shows.objects.get(id = show_id)
+        edit_show.title = request.POST['title']
+        edit_show.network = request.POST['network']
+        edit_show.description = request.POST['description']
+        edit_show.release_date = request.POST['release_date']
+        edit_show.save()
     return redirect(f'/{show_id}')
 
 def delete_warning(request, show_id):
